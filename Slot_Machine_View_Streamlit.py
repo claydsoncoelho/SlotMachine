79a27340b929f6684b595c3af59996f7d31fd668
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 00:45:53 +0000

    1

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import Slot_Machine_Model
 
 deposit = 1
 
@@ -7,3 +8,9 @@ st.write('The current balance is ', deposit)
 
 bet = st.slider('Bet amount', 0, int(deposit), 0)
 st.write(bet)
+
+slot_machine = Slot_Machine_Model.SlotMachine(deposit)
+slot_machine.bet = bet
+slot_machine.play_slot_machine()
+
+st.write(slot_machine.prize)
