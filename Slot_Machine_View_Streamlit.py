2891423fd775a9cd4fcc1e21eae2dd50b4398353
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 23:23:38 +0000

    asfa

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -11,24 +11,14 @@ def translate_symbol():
     slot_machine = st.session_state['slot_machine']
     display = ''
     new_symbols = [' :four_leaf_clover: ', ' :taco: ', ' :watermelon: ']
-    st.title(slot_machine.wheels_state)
-    st.title(set(slot_machine.wheels_state))
-    st.title(list(set(slot_machine.wheels_state)))
     key_list = list(set(slot_machine.wheels_state))
     symbols_dict = {}
 
     for i in range(len(key_list)):
         symbols_dict[key_list[i]] = new_symbols[i]
 
-    st.title(symbols_dict)
-
-    for i in range(slot_machine.number_of_wheels):
-        if slot_machine.wheels[i].current_symbol == 'A':
-            display += ' :four_leaf_clover: '
-        elif slot_machine.wheels[i].current_symbol == 'B':
-            display += ' :taco: '
-        else:
-            display += ' :watermelon: '
+    for symbol in slot_machine.wheels_state
+        display += symbols_dict[symbol]
         
     return display
 
