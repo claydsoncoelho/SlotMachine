f5a16e6c0201b8ea0c0045dd33c462c3d12eed04
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 13:10:08 +1300

    Update Slot_Machine_View_Streamlit.py

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-number = st.number_input('Depisit money')
+deposit = st.number_input('Depisit money')
 st.write('The current balance is ', number)
+
+bet = st.slider('Bet amount', 0, deposit, 0)
+st.write(bet)
