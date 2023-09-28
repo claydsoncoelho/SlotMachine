066a4ee4e05109d8d8442ee86fc8fe4e1c1f90c1
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 11:42:32 +0000

    f

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -24,7 +24,6 @@ def translate_symbol():
 
 def print_performance():
     slot_machine = st.session_state['slot_machine']
-    
     performance_container.write('Deposit: ' + str(slot_machine.initial_balance))
 
     if slot_machine.performance > 0:
@@ -49,17 +48,16 @@ def play():
     slot_machine = st.session_state['slot_machine']
 
     if bet > slot_machine.current_balance:
-        st.wirte('Bet cannot be bigger than balance!')
+        st.write('Bet cannot be bigger than balance!')
     else:
         slot_machine.bet = bet
         slot_machine.play_slot_machine()
         display_container.title(translate_symbol())
-        display_container.write('Prize:' + str(slot_machine.prize))
+        display_container.write('Prize: ' + str(slot_machine.prize))
 
     performance_container.write('Balance: ' + str(slot_machine.current_balance))
     print_performance()
 
-col1, col2, col3 = st.columns(3)
 
 # addin objects to controller container
 deposit = controller_container.number_input('Deposit money')
@@ -74,6 +72,3 @@ if controller_container.button('Bet'):
 if controller_container.button('Reset'):
     st.session_state['slot_machine'] = Slot_Machine_Model.SlotMachine(0)
     controller_container.write('Balance: ' + str(st.session_state['slot_machine'].current_balance))
-
-
-
