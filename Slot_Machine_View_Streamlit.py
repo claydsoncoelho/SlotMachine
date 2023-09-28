54b9dc910f7555bb95b4b82b9e1165c49e0b7c46
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 03:48:39 +0000

    DAS

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -36,9 +36,7 @@ def play():
                 "Deposit": 10,
                 "Balance": 20
             }
-)
-
-st.bar_chart(chart_data, x="Deposit", y="Balance")
+        st.bar_chart(chart_data, x="Deposit", y="Balance")
 
     st.title('Balance: ' + str(slot_machine.current_balance))
 
