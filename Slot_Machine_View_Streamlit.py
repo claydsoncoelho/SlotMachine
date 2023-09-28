9e666cb742be957b251a83dede75caa8732b1bdf
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 01:07:04 +0000

    fa

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -14,13 +14,13 @@ slot_machine.bet = bet
 slot_machine.play_slot_machine()
 
 if slot_machine.prize > 0:
-    text1 = ':green[' + str(slot_machine.wheels) + '] :sunglasses:'
+    text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
     text2 = 'Prize:' + str(slot_machine.prize)
     st.title('_Streamlit_ is :blue[cool] :sunglasses:')
     st.title(text1)
     st.title(text2)
 else:
-    text1 = ':red[' + str(slot_machine.wheels) + '] :snob:'
+    text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
     text2 = 'Prize:' + str(slot_machine.prize)
     st.title('_Streamlit_ is :red[cool] :sunglasses:')
     st.title(text1)
