595b17e6750a1377f1b199bdecc5d4581a4cfced
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 02:55:39 +0000

    fs

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -5,36 +5,27 @@ import Slot_Machine_Model
 def create_slot_machine(initial):
     if 'slot_machine' not in st.session_state:
         st.session_state['slot_machine'] = Slot_Machine_Model.SlotMachine(initial)
-    return Slot_Machine_Model.SlotMachine(initial)
 
+def play():
+    bet = st.number_input('Bet amount')
+    st.write('The current bet is ', bet)
+
+    if st.button('Bet'):
+        slot_machine = st.session_state['slot_machine']
+        slot_machine.bet = bet
+        slot_machine.play_slot_machine()
+        if slot_machine.prize > 0:
+            text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
+            text2 = 'Prize:' + str(slot_machine.prize)
+            st.title(text1)
+            st.title(text2)
+        else:
+            text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
+            text2 = 'Prize:' + str(slot_machine.prize)
+            st.title(text1)
+            st.title(text2)
 
 deposit = st.number_input('Deposit money')
 
 if st.button('Deposit'):
-    slot_machine = create_slot_machine(deposit)
-
-    bet = st.number_input('Bet amount')
-    st.write('The current bet is ', bet)
-
-    slot_machine.bet = bet
-    slot_machine.play_slot_machine()
-
-    if slot_machine.prize > 0:
-        text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
-        text2 = 'Prize:' + str(slot_machine.prize)
-        st.title(text1)
-        st.title(text2)
-    else:
-        text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
-        text2 = 'Prize:' + str(slot_machine.prize)
-        st.title(text1)
-        st.title(text2)
-
-
-    st.write(slot_machine)
-    st.write(slot_machine.bet)
-    st.write(slot_machine.current_balance)
-    st.write(slot_machine.initial_balance)
-    st.write(slot_machine.performance)
-    st.write(slot_machine.prize)
-    st.write(slot_machine.wheels)
+    create_slot_machine(deposit)
