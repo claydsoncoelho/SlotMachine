064177a9813035d6f6d2c171daaf8399e572962e
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 03:05:17 +0000

    sadfas

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -5,16 +5,6 @@ def play():
     slot_machine = st.session_state['slot_machine']
     slot_machine.bet = bet
     slot_machine.play_slot_machine()
-    if slot_machine.prize > 0:
-        text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
-        text2 = 'Prize:' + str(slot_machine.prize)
-        st.title(text1)
-        st.title(text2)
-    else:
-        text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
-        text2 = 'Prize:' + str(slot_machine.prize)
-        st.title(text1)
-        st.title(text2)
 
 
 def create_slot_machine(initial):
@@ -31,3 +21,18 @@ bet = st.number_input('Bet amount')
 
 if st.button('Bet'):
     play()
+
+if 'slot_machine' in st.session_state:
+    slot_machine = st.session_state['slot_machine']
+    if slot_machine.prize > 0:
+        text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
+        text2 = 'Prize:' + str(slot_machine.prize)
+        st.title(text1)
+        st.title(text2)
+    else:
+        text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
+        text2 = 'Prize:' + str(slot_machine.prize)
+        st.title(text1)
+        st.title(text2)
+    
+    st.write(slot_machine.current_balance)
