ca78e0d924865059b2ee022c75c4d4d9f91e8fc7
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 10:49:14 +0000

    re# Please enter the commit message for your changes. Lines starting

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -22,7 +22,7 @@ def translate_symbol():
 def print_performance():
     slot_machine = st.session_state['slot_machine']
     
-    balance_container.write('Deposit:', str(slot_machine.initial_balance))
+    balance_container.write('Deposit: ' + str(slot_machine.initial_balance))
 
     if slot_machine.performance > 0:
         text1 = f'Performance: :green[{slot_machine.performance:.2f}] % :sunglasses:'
