a902f5b4d86fe9610a68b6d6f64075ef67c473fc
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 11:45:54 +0000

    ds

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -48,7 +48,7 @@ def play():
     slot_machine = st.session_state['slot_machine']
 
     if bet > slot_machine.current_balance:
-        st.write('Bet cannot be bigger than balance!')
+        st.error('Bet cannot be bigger than balance!')
     else:
         slot_machine.bet = bet
         slot_machine.play_slot_machine()
