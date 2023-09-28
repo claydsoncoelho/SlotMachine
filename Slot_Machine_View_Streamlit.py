d30cc1fc289a4029582861ecb2671e0707d57f68
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 00:58:44 +0000

    da

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -13,4 +13,11 @@ slot_machine = Slot_Machine_Model.SlotMachine(deposit)
 slot_machine.bet = bet
 slot_machine.play_slot_machine()
 
-st.write(slot_machine.prize)
+if slot_machine.prize > 0:
+    st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+    st.title(':green['+'slot_machine.wheels' + '] :sunglasses:')
+    st.title('Prize:', slot_machine.prize)
+else:
+    st.title('_Streamlit_ is :red[cool] :sunglasses:')
+    st.title(':red['+'slot_machine.wheels' + '] :sob:')
+    st.title('Prize:', slot_machine.prize)
