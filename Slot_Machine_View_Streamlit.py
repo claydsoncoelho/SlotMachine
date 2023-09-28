c82ca29468121c6078a47792fa7e655f47e7f0d2
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-09-28 03:14:57 +0000

    fdasdfs

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -1,15 +1,26 @@
 import streamlit as st
 import Slot_Machine_Model
 
+def create_slot_machine(initial):
+    if 'slot_machine' not in st.session_state:
+        st.session_state['slot_machine'] = Slot_Machine_Model.SlotMachine(initial)
+
 def play():
     slot_machine = st.session_state['slot_machine']
     slot_machine.bet = bet
     slot_machine.play_slot_machine()
+    if slot_machine.prize > 0:
+        text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
+        text2 = 'Prize:' + str(slot_machine.prize)
+        st.title(text1)
+        st.title(text2)
+    else:
+        text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
+        text2 = 'Prize:' + str(slot_machine.prize)
+        st.title(text1)
+        st.title(text2)
 
-
-def create_slot_machine(initial):
-    if 'slot_machine' not in st.session_state:
-        st.session_state['slot_machine'] = Slot_Machine_Model.SlotMachine(initial)
+    st.title('Balance:', slot_machine.balance)
 
 
 deposit = st.number_input('Deposit money')
@@ -22,20 +33,6 @@ bet = st.number_input('Bet amount')
 if st.button('Bet'):
     play()
 
-if 'slot_machine' in st.session_state:
-    slot_machine = st.session_state['slot_machine']
-    if slot_machine.prize > 0:
-        text1 = ':green[' + str(slot_machine.wheels_state) + '] :sunglasses:'
-        text2 = 'Prize:' + str(slot_machine.prize)
-        st.title(text1)
-        st.title(text2)
-    else:
-        text1 = ':red[' + str(slot_machine.wheels_state) + '] :sob:'
-        text2 = 'Prize:' + str(slot_machine.prize)
-        st.title(text1)
-        st.title(text2)
-    
-    st.write(slot_machine.current_balance)
 
-    if st.button('Reset'):
-        st.session_state['slot_machine'] = Slot_Machine_Model.SlotMachine(deposit)
+if st.button('Reset'):
+    st.session_state['slot_machine'] = Slot_Machine_Model.SlotMachine(deposit)
