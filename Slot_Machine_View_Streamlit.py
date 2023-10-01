d417f1843a0cf57246983d965f77e7f6104f4884
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2023-10-01 21:24:14 +1300

    sd

--- Slot_Machine_View_Streamlit.py
+++ Slot_Machine_View_Streamlit.py
@@ -57,6 +57,8 @@ def make_deposit(initial):
     else:
         st.session_state['slot_machine'].initial_balance += initial
         st.session_state['slot_machine'].current_balance += initial
+        temp = col2.title
+        col2.tile = temp
 
     performance_container.write('Balance: ' + str(st.session_state['slot_machine'].current_balance))
 
